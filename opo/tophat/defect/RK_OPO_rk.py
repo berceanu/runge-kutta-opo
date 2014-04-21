5f71ec079e7ecf9ad0248a5862ff2599d36faa08
Andrei Berceanu <berceanu@runbox.com>
2014-04-21 17:12:03 +0200

    transfered main file from ipython

--- opo/tophat/defect/RK_OPO_rk.py
+++ opo/tophat/defect/RK_OPO_rk.py
@@ -1,20 +1,22 @@
-##########################
-# global variables
-##########################
-
 from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import ConfigParser
+import init_pump
+import kinetosis
 
-def read_parameters(filename):
-    config = ConfigParser.RawConfigParser()
+
+def find_index(dist_or_mom, side, delta):
+    return int(np.around((side + dist_or_mom) / delta))
+
+
+def read_input(filename):
+    param = ConfigParser.RawConfigParser()
     try:
-        config.readfp(open(filename))
+        param.readfp(open(filename))
     except IOError:
-        raise IOError('cannot find INPUT.ini, exiting')
-    return config
-
-def read_input():
-    param = read_parameters("INPUT.ini")
+        raise IOError('cannot find {0:s}, exiting'.format(filename))
 
     kappa_C   = param.getfloat("params", "kappa_C")
     kappa_X   = param.getfloat("params", "kappa_X")
@@ -35,89 +37,74 @@ def read_input():
     def_y_pos = param.getfloat("params", "def_y_pos")
     gv        = param.getfloat("params", "gv")
 
-    ax = 2 * Lx / Nx    
-    ay = 2 * Ly / Ny    
-    norm = ax * ay    
     norm_c = np.sqrt(Nx*Ny)/256/np.sqrt(Lx*Ly)*70
     f_p = f_p / norm_c
-    return
 
-def init_pot_c():
-    #for old coordinates of (Nx/2,Ny/2) set def_x_pos = -ax, def_y_pos=-ay
-    ix = int((def_x_pos + Lx) / ax + 1)
-    iy = int((def_y_pos + Ly) / ay + 1)
-
-    pot_c = 0
-    pot_c(ix,iy)=gv
-    return
-
-def init_pump_th():
-    !top hat pump    
-
-    do iy=1, Ny    
-       sy=-Ly+(iy-1)*ay    
-       do ix=1, Nx    
-          sx=-Lx+(ix-1)*ax    
-          pump_spatial(ix,iy)=f_p*0.5*&    
-               ( tanh((1.0_dp/10)*( sqrt(sx**2+sy**2)+sigma_p ))-&    
-               tanh((1.0_dp/10)*( sqrt(sx**2+sy**2)-sigma_p )) ) + zero    
-       end do    
-    end do    
-        
-    do iy=1, Ny    
-       #sy=-Ly+(iy-1)*ay    
-       do ix=1, Nx    
-          sx=-Lx+(ix-1)*ax    
-          pump_spatial(ix,iy)= pump_spatial(ix,iy)*cos(k_p*sx)+I*pump_spatial(ix,iy)*sin(k_p*sx)    
-       end do    
-    end do
-    return
-
-def setg():
-    DO j=1,(Ny/2+1)    
-       DO k=1,(Nx/2+1)    
-          kinetic(k,j)=pi**2*(&    
-               &(k-1)**2/(Lx**2)+(j-1)**2/(Ly**2))    
-       END DO    
-    END DO    
-    DO j=(Ny/2+2),Ny    
-       DO k=(Nx/2+2),Nx    
-          kinetic(k,j)=pi**2*( &    
-               & (k-1-Nx)**2/(Lx**2)+(j-1-Ny)**2/(Ly**2))    
-       END DO    
-    END DO    
-    DO j=1,(Ny/2+1)    
-       DO k=(Nx/2+2),Nx    
-          kinetic(k,j)=pi**2*(&    
-               &(k-1-Nx)**2/(Lx**2)+(j-1)**2/(Ly**2))    
-       END DO    
-    END DO    
-    DO j=(Ny/2+2),Ny    
-       DO k=1,(Nx/2+1)      
-          kinetic(k,j)=pi**2*(&    
-               &(k-1)**2/(Lx**2)+(j-1-Ny)**2/(Ly**2))    
-       END DO    
-    END DO    
-    return
+    return (kappa_C, kappa_X, delta, k_p, omega_p,
+		    sigma_p, f_p, Lx, Ly, Nx, Ny, Nt,
+		    tot_h, dxsav_rk, eps_r, def_x_pos, def_y_pos, gv)
 
 
-def main():
-
-    read_input()
-    init_pot_c()
-
-    pdb=0
-
-    init_pump_th()
-    init_pump_th()
-
-    setg()
-        
+def init_pot_c(strength, defypos, defxpos, ly, lx, ny, nx, ay, ax):
+    iy = find_index(defypos, ly, ay)
+    ix = find_index(defxpos, lx, ax)
+    potential = np.zeros((ny, nx), dtype=np.complex128)
+    potential[iy, ix] = strength
+    return potential
 
 
-    x1_r=0
-    x2_r=tot_h
-    h1_r=0.001_dp
-    hmin_r=0
+def init_pump_th(fp, sigmap, kp, Y, X):
+    #top hat pump
+    r = np.sqrt(X**2 + Y**2)
+    return 0.5 * fp * \
+	    (np.tanh(1 / 10 * (r + sigmap)) - np.tanh(1 / 10 * (r - sigmap)))\
+	    * np.exp(1j * kp * X)
 
-    odeint_rk(pdb,x1_r,x2_r,eps_r,h1_r,hmin_r)
+
+def setg(ny, nx, KY, KX):
+    M = KY**2 + KX**2
+    for y in xrange(0, int(ny), int(ny/2)):
+        for x in xrange(0, int(nx), int(nx/2)):
+            view = M[y:y+int(ny/2), x:x+int(nx/2)]
+	    view[:,:] = np.fft.fftshift(view)
+	    print view
+    return M
+
+
+#def main():
+kappa_C, kappa_X, delta, k_p, omega_p,\
+sigma_p, f_p, Lx, Ly, Nx, Ny, Nt,\
+tot_h, dxsav_rk, eps_r, def_x_pos, def_y_pos, gv = read_input('INPUT.ini')
+
+y, a_y = np.linspace(-Ly, Ly, num=Ny, endpoint=False, retstep=True)
+x, a_x = np.linspace(-Lx, Lx, num=Nx, endpoint=False, retstep=True)
+#norm = a_x * a_y
+X, Y = np.meshgrid(x, y)
+
+side_ky = np.pi / a_y
+side_kx = np.pi / a_x
+ky, delta_ky = np.linspace(-(2 * (Ny - 1) / float(Ny) - 1) * side_ky, side_ky, num=Ny, retstep=True)
+kx, delta_kx = np.linspace(-(2 * (Nx - 1) / float(Nx) - 1) * side_kx, side_kx, num=Nx, retstep=True)
+KX, KY = np.meshgrid(kx, ky)
+
+pot_c = init_pot_c(gv, def_y_pos, def_x_pos, Ly, Lx, Ny, Nx, a_y, a_x)
+pdb = np.zeros((Ny, Nx, 2), dtype=np.complex128)
+
+pump_spatial = init_pump_th(f_p, sigma_p, k_p, Y, X)
+
+kinetic = setg(Ny, Nx, KY, KX)
+
+x1_r=0
+x2_r=tot_h
+h1_r=0.001
+hmin_r=0
+
+#odeint_rk(pdb,x1_r,x2_r,eps_r,h1_r,hmin_r)
+fortran_pump = np.zeros((Ny, Nx), dtype=np.complex128)
+fortran_pump = init_pump.init_pump_th(Nx,Ny,Lx,Ly,a_x,a_y,f_p,sigma_p,k_p)
+cinetic = kinetosis.setg(Nx,Ny,Lx,Ly)
+print 'done'
+
+
+#if __name__ == "__main__":
+    #main()
